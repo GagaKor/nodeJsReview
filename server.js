0a3f299f1706427e9df1d8c0ff61d61d5f41f272
Gagakor <wexdaq554@gmail.com>
2021-07-21 23:50:30 +0900

    get 방식 연습

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
+var { response } = require('express');
 const express = require('express');
 const app = express();
 
-app.listen();
+app.listen(8080, function(){
+    console.log('node Js 서버 시작 포트 8080')
+});
+app.get('/', function(reqeust, response){
+    response.sendFile(__dirname+'/index.html')
+});
+app.get('/beauty', function(reqeust, response){
+    response.send('뷰티용품 페이지')
+});
+app.get('/pet', function(reqeust, response){
+    response.send('펫 용품 페이지')
+});
