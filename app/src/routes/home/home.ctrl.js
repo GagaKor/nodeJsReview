c7adca0377428423dcc6f3b4c14c252b1cd49f4f
Gagakor <wexdaq554@gmail.com>
2021-07-23 14:45:13 +0900

    로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const users = {
+  id : ["1234", "asdf"],
+  password : ["1234" , "asdf"],
+};
+
 //contoller 부분
 const output = {
     home: (req, res) => {
@@ -11,7 +16,21 @@ const output = {
   };
   const process = {
     login: (req, res) => {
-        console.log(req.body);
+       const id = req.body.id,
+        password = req.body.password;
+
+       if(users.id.includes(id)){
+        const idx = users.id.indexOf(id);
+        if(users.password[idx] === password){
+          return res.json({
+            success : true,
+          });
+         }
+       }
+       return res.json({
+         success : false,
+         msg : "로그인 실패"
+       });
     }
     };
   
